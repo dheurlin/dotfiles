2142faf6fad34113da0eba49078261dddff840e3
dheurlin <daniel.heurlin@gmail.com>
2018-01-15 19:39:43 +0100

    Blabla

--- path.sh
+++ path.sh
@@ -2,6 +2,7 @@
 export PATH=/Applications/codelite.app/Contents/SharedSupport:$HOME/arm-none-eabi/bin:$PATH
 export PATH=/Applications/XAMPP/xamppfiles:$PATH
 export PATH=~/.symlinks:$PATH
+export PATH=~/.local/bin:$PATH
 export PATH="/usr/local/sbin:$PATH"
 export PATH="$HOME/Library/Haskell/bin:$PATH" # add cabal packages
 export PATH="/Applications/MacVim.app/Contents/bin:$PATH" # Add mac vim
@@ -10,4 +11,4 @@ export PATH="/Users/danielheurlin/.erlangInstaller/20.0/bin:$PATH" # Add erlang
 export PATH="/Users/danielheurlin/.userbin:$PATH" # Add erlang
 export PATH="/usr/local/opt/ncurses/bin:$PATH"
 # export PATH="/Applications/Deluge.app/Contents/MacOS:$PATH" # Add Deluge
-export PATH="/Applications/MATLAB_R2016b.app/bin:$PATH" # Add matlab binarys
+export PATH="/Applications/MATLAB_R2016b.app/bin:$PATH" # Add matlab binaies
