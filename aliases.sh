12dfd86b00ea01e0fe0deb199900e73601d26660
Daniel Heurlin <daniel.heurlin@gmail.com>
2018-02-17 23:50:44 +0100

    Make functions for controlling gnome-night-light more easily

--- aliases.sh
+++ aliases.sh
@@ -23,7 +23,7 @@ alias pmd-to-master='~/Dropbox/coding\ stuff/bash/pmds-to-master-pdf.sh'
 yt-sync() { ~/Dropbox/coding\ stuff/bash/sync-youtube-playlist.sh "$@" }
 
 # Vim should be the macvim executable
-alias vim="/usr/local/Cellar/macvim/8.0-144_3/MacVim.app/Contents/bin/vim"
+# alias vim="/usr/local/Cellar/macvim/8.0-144_3/MacVim.app/Contents/bin/vim"
 
 # A shortcut to edit .zshrc
 alias zrc="vim ~/dotfiles/.zshrc"
@@ -61,4 +61,44 @@ zath() {
 # shortcut for sleeping display
 ds(){
     pmset displaysleepnow
+}
+
+## Aliases for controlling gnome night light
+
+# A global variable keeping thrack of the current temp
+export PUSHED_NL_TEMP=${PUSHED_NL_TEMP:-5000}
+
+# get the current temp
+nl-get-curr() {
+    # get the current value (will return "uint32 XXXX")
+    curr=$(gsettings get org.gnome.settings-daemon.plugins.color night-light-temperature)
+    # extract the temperature (get the XXXX from above) and store it in a global variable
+    echo $(echo $curr | cut -d " " -f 2)
+}
+
+## Open gnome-night-light-adjuster
+set-nla-alias() {
+    alias nla="gnome-night-light-adjuster"
+}
+
+set-nla-alias
+
+## Disables the night light, after saving the current temperature to a variable
+# so that it can be restored later
+nld() {
+    # Save the current temp
+    export PUSHED_NL_TEMP=$(nl-get-curr)
+
+    # Set the temp to minimum
+    gnome-night-light-adjuster 6500 &>/dev/null
+
+    # temporarily disable nla command to avoid interference
+    alias nla="echo 'Re-enable night light by running nlr before adjusting temperature!'"
+}
+
+## Restores the night light to what is was before nld was called
+nlr() {
+    gnome-night-light-adjuster $PUSHED_NL_TEMP &>/dev/null
+    # re-enable nla alias
+    set-nla-alias
 }
