d7ae29bb8517be50c0d7b171162a66cddd3d7fe7
Daniel Heurlin <daniel.heurlin@gmail.com>
2018-11-10 00:42:37 +0100

    Add aliases for exa in zsh

--- aliases.sh
+++ aliases.sh
@@ -2,6 +2,12 @@
 alias gh="cd ~"
 alias gdoc="cd ~/Dokument"
 
+## Shortcuts for exa
+# (letters are chosen to be analagous to l and ls)
+alias eh="exa --header --long --grid --git"
+alias e="eh -a"
+alias es="exa"
+
 
 # A shortcut to edit i3 config
 alias i3c="cd ~/.config/i3 && vim config"
@@ -12,6 +18,7 @@ alias lolcow="fortune | cowsay | lolcat"
 # Reload the keyboard conf
 alias kbconf="~/.config/autostart/kbd-setup.sh"
 
+# Aliases for starting different window managers/desktop environments
 alias i3-start="startx ~/.xinitrc i3"
 alias gnome-start="startx ~/.xinitrc gnome"
 alias xmonad-start="startx ~/.xinitrc xmonad"
